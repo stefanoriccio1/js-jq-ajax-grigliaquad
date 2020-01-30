6945632b26c163283d8be9005f836e0428ffd0dc
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-30 16:49:10 +0100

    aggiunta funzione per aggiungere classe colore al box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
-
-
-})
+  $(document).on('click', '.box', function(){
+    if($(this).hasClass('yellow')== false){
+      $(this).addClass('yellow');
+    }
+});
+});
